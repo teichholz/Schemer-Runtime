b6e3e578264c252976a15fb7727d365cd549d4f5
Tim Eichholz <tim.eichholz98@web.de>
2021-04-23 14:56:11 +0000

    fixup

--- hello.nix
+++
@@ -1,9 +0,0 @@
-with (import <nixpkgs> {});
-
-stdenv.mkDerivation {
-  name = "hello";
-  src = fetchurl {
-    url = "http://www.example.org/hello.tar.gz";
-    sha256 = "1111111111111111111111111111111111111111111111111111";
-  };
-}
